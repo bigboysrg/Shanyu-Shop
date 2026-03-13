be21cf6a449ad8dd9b6c17c7b14ef0990716b6c2
SergeBarcelon <202310658@fit.edu.ph>
2026-03-13 19:26:34 +0800

    Initial commit of Shanyu Shop

---
+++ app/Views/user_catalog.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Catalog</title>
+    <link rel="stylesheet" href="<?= base_url('css/style.css'); ?>">
+</head>
+<body>
+    <h1>Our Product Catalog</h1>
+    <div class="catalog-container">
+        <p>Welcome to our store! Browse our latest collection.</p>
+    </div>
+</body>
+</html>
