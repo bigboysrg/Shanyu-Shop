95ecea757cb4e476e41391492d729dabd54afae9
SergeBarcelon <202310658@fit.edu.ph>
2026-03-13 23:01:56 +0800

    added dashboard and fixed registration

--- app/Views/register_view.php
+++ app/Views/register_view.php
@@ -1,14 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link rel="stylesheet" href="<?= base_url('css/topbar.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/register.css') ?>">
 </head>
 <body>
-    <div class="register-card">
-        <h1>HELLO!</h1>
-        <?= form_open('register/store') ?>
-            <input type="text" name="name" placeholder="Username" required>
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit">Sign Up</button>
-        <?= form_close() ?>
+
+    <?= view('topbar') ?>
+
+    <div class="register-container">
+        <div class="register-card">
+            <h1>Register</h1>
+            <?= form_open('register/store') ?>
+                <input type="text" name="name" placeholder="Username" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Sign Up</button>
+            <?= form_close() ?>
+        </div>
     </div>
+
 </body>
+</html>
---
+++ app/Views/topbar.php
@@ -0,0 +1,14 @@
+<nav class="top-nav">
+    <div class="nav-left">
+        <div class="avatar-large"></div>
+        <div class="user-info">
+            <span class="name">Shan yu</span>
+        </div>
+    </div>
+
+    <div class="nav-right">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact</a>
+    </div>
+</nav>
